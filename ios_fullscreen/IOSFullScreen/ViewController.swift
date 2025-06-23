fb97638b92c84839354f74ecc188987b6521b61c
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2025-06-23 15:54:53 +0200

    Fullscreen controller

--- ios_fullscreen/IOSFullScreen/ViewController.swift
+++ ios_fullscreen/IOSFullScreen/ViewController.swift
@@ -10,6 +10,7 @@ class ViewController: UIViewController {
     @IBAction func buttonWasTapped(_ sender: Any) {
         if let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine {
             let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+            flutterViewController.modalPresentationStyle = .fullScreen
             self.present(flutterViewController, animated: true, completion: nil)
         }
     }
